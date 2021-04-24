7a611ebad9c58a7d2141d5fdac8a3e424375cbd4
Alfie Darko <me@alfiedarko.co.uk>
2021-04-24 15:37:36 +0100

    favicon update

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import "../styles/globals.css";
 
 import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <ChakraProvider>
+        <Head>
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
+
         <Component {...pageProps} />
       </ChakraProvider>
     </>
